327be5b5b686fa71f6203bdcee1d98d65e45c813
bcprods <briancoxxie3@gmail.com>
2017-11-28 00:16:36 -0800

    fixing null phone number

--- php-cgi/labdashboard.js
+++ php-cgi/labdashboard.js
@@ -1,7 +1,7 @@
 function formatPhoneNumber(s) {
   var s2 = (""+s).replace(/\D/g, '');
   var m = s2.match(/^(\d{3})(\d{3})(\d{4})$/);
-  return (!m) ? null : "(" + m[1] + ") " + m[2] + "-" + m[3];
+  return (!m) ? s : "(" + m[1] + ") " + m[2] + "-" + m[3];
 }
 
 function initLabDashboard() {
